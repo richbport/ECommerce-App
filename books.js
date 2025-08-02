9d9fa3b9b525fcfeb8dea1b52c6c384915286054
Rich <richbport@yahoo.com>
2025-08-02 15:32:08 -0400

    select adjust

--- books.js
+++ books.js
@@ -3,13 +3,13 @@ let books;
 async function renderBooks(filter) {
   const booksWrapper = document.querySelector(".books");
 
-  booksWrapper.classList += ' books__loading'
+  booksWrapper.classList += " books__loading";
 
   if (!books) {
     books = await getBooks();
   }
-  
-  booksWrapper.classList.remove('books__loading')
+
+  booksWrapper.classList.remove("books__loading");
 
   if (filter === "LOW_TO_HIGH") {
     books.sort(
